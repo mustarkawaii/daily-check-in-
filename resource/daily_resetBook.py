17d62f9f5b948b6573b78107a2a299af7805040d
木石MuSTAR <74960289+mustarkawaii@users.noreply.github.com>
2022-07-20 16:13:23 +0800

    Update daily_resetBook.py

--- resource/daily_resetBook.py
+++ resource/daily_resetBook.py
@@ -17,7 +17,7 @@ print("正在加载签到模块...")
 time.sleep(3)
 print("请求签到中...")
 url='http://wheremylife.cn/1.0/user/resetBook'
-headers={'User-Agent':'Mozilla/5.0(WindowsNT10.0;Win64;x64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/103.0.5060.114Safari/537.36Edg/103.0.1264.49','Accept':'*/*','Accept-Encoding':'gzip,deflate','Accept-Language':'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6','Cache-Control':'no-cache','Content-Length':'2','Content-Type':'application/json','Cookie':'wml:key=eyJldmVudHMiOltdLCJ1c2VyaWQiOjM1NjQ1LCJfZXhwaXJlIjoxNjU4MTQzMDc0MTE5LCJfbWF4QWdlIjo2MDQ4MDAwMDB9; wml:key.sig=b4Q0_ZWvkRkZ5l3xuU3fuIOW6SE','DNT':'1','Host':'wheremylife.cn','Origin':'http://wheremylife.cn','Pragma':'no-cache','Proxy-Connection':'keep-alive','Referer':'http://wheremylife.cn/home'}
+headers={'User-Agent':'Mozilla/5.0(WindowsNT10.0;Win64;x64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/103.0.5060.114Safari/537.36Edg/103.0.1264.49','Accept':'*/*','Accept-Encoding':'gzip,deflate','Accept-Language':'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6','Cache-Control':'no-cache','Content-Length':'2','Content-Type':'application/json','Cookie':'wml:key=eyJldmVudHMiOltdLCJ1c2VyaWQiOjM1NjQ1LCJfZXhwaXJlIjoxNjU4OTA5NDcxNDYyLCJfbWF4QWdlIjo2MDQ4MDAwMDB9; wml:key.sig=OKaY20e-GBLR4XmkiWkuFuJtoyE','DNT':'1','Host':'wheremylife.cn','Origin':'http://wheremylife.cn','Pragma':'no-cache','Proxy-Connection':'keep-alive','Referer':'http://wheremylife.cn/home'}
 a={"username": "3031147044@qq.com", "password": "MuSTAR1234"}
 x=requests.post(url, headers=headers, json=a,timeout=2.50)
 time.sleep(5)
@@ -43,7 +43,7 @@ while re_try_time==1:
         result="WhereMyLife打卡成功！剩余重置天数为" + str(14) + "天"
     else:
         
-        result="WhereMyLife打卡失败，原因是【" + x.text  + "】且再次尝试仍失败，请检查登陆cookies后再试，剩余重置天数为" + str(day-1)
+        result="WhereMyLife打卡失败，原因是【" + x.text  + "】且再次尝试仍失败，请检查登陆cookies后再试" 
         
 
 print(result)
